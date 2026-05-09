7eaffe574a176ec00fe2f5c68976db6c87f0672d
degnisDev <degnisdev@gmail.com>
2026-05-09 00:42:51 -0500

    Finalizamos la primer Activity, empezamos la estructura de la segunda

--- Desktop/INGENIERIA DE SOFTWARE/5 - SEMESTRE/APLICACIONES_MOVILES/myApol_App/app/src/main/java/com/example/myaple_app/RegisterActivity.kt
+++ Desktop/INGENIERIA DE SOFTWARE/5 - SEMESTRE/APLICACIONES_MOVILES/myApol_App/app/src/main/java/com/example/myaple_app/RegisterActivity.kt
@@ -1,30 +1,20 @@
 package com.example.myaple_app
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-//        Configuramos el enlace que nos lleva a la actividad de registro
-        val btnRegister = findViewById<TextView>(R.id.tvSignUp)
-        btnRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
         }
     }
 }
