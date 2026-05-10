9bdb5370ef3b308db98b0d7cce158b0d64a039ff
degnisDev <degnisdev@gmail.com>
2026-05-10 17:18:26 -0500

    Se creo la activity de AdminDasboard tomando como base la de profile

--- Desktop/INGENIERIA DE SOFTWARE/5 - SEMESTRE/APLICACIONES_MOVILES/myApol_App/app/src/main/java/com/example/myaple_app/AdminDashboardActivity.kt
+++ Desktop/INGENIERIA DE SOFTWARE/5 - SEMESTRE/APLICACIONES_MOVILES/myApol_App/app/src/main/java/com/example/myaple_app/AdminDashboardActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myaple_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,18 @@ class AdminDashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_admin_dashboard)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Lógica de Logout (Reutilizada de ProfileActivity)
+        findViewById<LinearLayout>(R.id.btnLogoutAction).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
 }
