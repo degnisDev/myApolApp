55f540beee946fe1c842748a5ca379df70729afe
degnisDev <degnisdev@gmail.com>
2026-05-10 17:13:52 -0500

    Empezamos con la creacion de las activityes del proximo rol, el Admin, para lo que creamos textos, imagenes

--- Desktop/INGENIERIA DE SOFTWARE/5 - SEMESTRE/APLICACIONES_MOVILES/myApol_App/app/src/main/java/com/example/myaple_app/MainActivity.kt
+++ Desktop/INGENIERIA DE SOFTWARE/5 - SEMESTRE/APLICACIONES_MOVILES/myApol_App/app/src/main/java/com/example/myaple_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myaple_app
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -32,6 +33,13 @@ class MainActivity : AppCompatActivity() {
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         btnLogin.setOnClickListener {
             val intent = Intent(this, CatalogActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Navegación al Dashboard de Administrador
+        val btnAdminLogin = findViewById<LinearLayout>(R.id.btnAdminLogin)
+        btnAdminLogin.setOnClickListener {
+            val intent = Intent(this, AdminDashboardActivity::class.java)
             startActivity(intent)
         }
     }
