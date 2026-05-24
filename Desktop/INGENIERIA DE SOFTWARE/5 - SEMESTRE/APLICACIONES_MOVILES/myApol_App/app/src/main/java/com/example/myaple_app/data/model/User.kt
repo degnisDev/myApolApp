be76859f288bf72faa5aec201d3414051835c4f0
degnisDev <degnisdev@gmail.com>
2026-05-24 14:27:14 -0500

    Creamos una nueva columna en la DB para agregar los roles, los cuales se daran desde el sistema

--- Desktop/INGENIERIA DE SOFTWARE/5 - SEMESTRE/APLICACIONES_MOVILES/myApol_App/app/src/main/java/com/example/myaple_app/data/model/User.kt
+++ Desktop/INGENIERIA DE SOFTWARE/5 - SEMESTRE/APLICACIONES_MOVILES/myApol_App/app/src/main/java/com/example/myaple_app/data/model/User.kt
@@ -2,11 +2,11 @@ package com.example.myaple_app.data.model
 
 import kotlinx.serialization.Serializable
 
-
 @Serializable
 data class User(
     val id: String,
     val name: String,
     val email: String,
     val phone: String,
-)
+    val role: String = "client" // Nuevo campo para control de acceso
+) : java.io.Serializable
